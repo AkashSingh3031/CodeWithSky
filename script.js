378b02fd7efe81d32b714e29ad0ac500d7586d75
Akash Singh <60146338+AkashSingh3031@users.noreply.github.com>
2021-10-06 00:33:40 +0530

    remove SME

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function () {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "YouTuber", "Blogger", "Subject Matter Expert in Computer Science"],
+        strings: ["Student", "YouTuber", "Blogger"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "YouTuber", "Blogger", "Subject Matter Expert in Computer Science"],
+        strings: ["Student", "YouTuber", "Blogger"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
